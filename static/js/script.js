9844d20e1ac16be3d318f3a98c9b01e4fce962c4
A D E L M A H M O U D <adelam578568@gmail.com>
2024-12-02 17:36:59 +0200

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function resetProgressStatus() {
-    fetch('https://subtranscribe.koyeb.app/reset-progress', { method: 'GET' })
+    fetch('/reset-progress', { method: 'GET' })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok.');
